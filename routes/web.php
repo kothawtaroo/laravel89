3c02f7b80238e1569a08b9b5760cc4885896f09d
TTO <myatthuta2019@gmail.com>
2023-07-16 00:03:24 +0630

    migrate ready

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Livewire\AboutComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\ContactComponent;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +19,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',HomeComponent::class)->name('home.index');
+Route::get('/about',AboutComponent::class)->name('about');
+Route::get('/contact',ContactComponent::class)->name('contact');
+Route::get('/shop',ShopComponent::class)->name('shop');
+Route::get('/cart',CartComponent::class)->name('shop.cart');
+Route::get('/checkout',CheckoutComponent::class)->name('shop.checkout');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
