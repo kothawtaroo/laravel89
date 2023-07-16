6b04149094c395727bc11c208d54a533b4464453
TTO <myatthuta2019@gmail.com>
2023-07-16 08:30:26 +0630

    e

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,21 +71,7 @@
 													<li class="menu-item" >
 														<a title="Dashboard" href="{{ route('admin.dashboard') }}">Dashboard</a>
 													</li>
-													<li class="menu-item">
-														<a title="Categories" href="{{ route('admin.categories') }}">Categories</a>
-													</li>
-													<li class="menu-item">
-														<a title="Products" href="{{ route('admin.products') }}">All Products</a>
-													</li>
-													<li class="menu-item">
-														<a title="Manage Home Slider" href="{{ route('admin.homeslider') }}">Manage Home Slider</a>
-													</li>
-													<li class="menu-item">
-														<a title="Manage Home Categories" href="{{ route('admin.homecategories') }}">Manage Home Categories</a>
-													</li>
-													<li class="menu-item">
-														<a title="Sale Setting" href="{{ route('admin.sale') }}">Sale Setting</a>
-													</li>
+													
 													<li class="menu-item">
 														<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
 													</li>
